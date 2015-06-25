dea2b961a39efe127e3a9ddf2e587cb76256f236
jnf <jeremy.flores@gmail.com>
2015-06-25 11:24:57 -0700

    Use my csv files to seed my db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+CSV.foreach("db/seed_csvs/markets.csv", headers: [:id, :name, :address, :county, :city, :state, :postal_code]) do |row|
+  row_hash = row.to_h
+  row_hash.delete :id
+  Market.create(row_hash)
+end
+
+CSV.foreach("db/seed_csvs/vendors.csv", headers: [:id, :name, :employees, :market_id]) do |row|
+  row_hash = row.to_h
+  row_hash.delete :id
+  Vendor.create(row_hash)
+end
+
+CSV.foreach("db/seed_csvs/products.csv", headers: [:id, :name, :vendor_id]) do |row|
+  row_hash = row.to_h
+  row_hash.delete :id
+  Product.create(row_hash)
+end
+
+CSV.foreach("db/seed_csvs/sales.csv", headers: [:id, :amount, :purchased_at, :vendor_id, :product_id]) do |row|
+  row_hash = row.to_h
+  row_hash.delete :id
+  Sale.create(row_hash)
+end
